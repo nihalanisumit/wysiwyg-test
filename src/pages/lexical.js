552be47d87babd0127bf042b3f70bcd20efc7d37
Sumit Nihalani <nihalanisumit@gmail.com>
2022-09-21 03:05:33 +0530

    playing around with lexical

--- src/pages/lexical.js
+++ src/pages/lexical.js
@@ -14,12 +14,17 @@ import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin'
 import { ListPlugin } from '@lexical/react/LexicalListPlugin'
 import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin'
 import { TRANSFORMERS } from '@lexical/markdown'
+import { $generateHtmlFromNodes } from '@lexical/html'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
+import { $getRoot, $getSelection } from 'lexical'
 
 import ListMaxIndentLevelPlugin from 'lexical/plugins/ListMaxIndentLevelPlugin'
 import CodeHighlightPlugin from 'lexical/plugins/CodeHighlightPlugin'
 import AutoLinkPlugin from 'lexical/plugins/AutoLinkPlugin'
 import exampleTheme from 'lexical/themes/ExampleTheme'
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Stack } from '@chakra-ui/react'
+import { useState } from 'react'
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>
@@ -48,29 +53,51 @@ const editorConfig = {
   ],
 }
 
+function SaveButton() {}
+
 export default function Editor() {
+  const [editable, setEditable] = useState(true)
+  const [rootNote, setRootNode] = useState(null)
+  const [content, saveContent] = useState('')
+  const [globalEditor, setGlobalEditor] = useState(null)
+
+  function toggleSave() {
+    // globalEditor.setEditable(!editable)
+    setEditable(!editable)
+  }
+
+  function onChange(state, editor) {
+    state.read(() => {
+      const root = $getRoot()
+      setRootNode(root)
+    })
+  }
+
   return (
-    // <Box width="50vw">
-    <LexicalComposer initialConfig={editorConfig}>
-      <div className="editor-container">
-        <ToolbarPlugin />
-        <div className="editor-inner">
-          <RichTextPlugin
-            contentEditable={<ContentEditable className="editor-input" />}
-            placeholder={<Placeholder />}
-          />
-          <HistoryPlugin />
-          <TreeViewPlugin />
-          <AutoFocusPlugin />
-          <CodeHighlightPlugin />
-          <ListPlugin />
-          <LinkPlugin />
-          <AutoLinkPlugin />
-          <ListMaxIndentLevelPlugin maxDepth={7} />
-          <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+    <Stack align="center">
+      <LexicalComposer initialConfig={{ ...editorConfig, editable }}>
+        <div className="editor-container">
+          <ToolbarPlugin />
+          <div className="editor-inner">
+            <RichTextPlugin
+              contentEditable={<ContentEditable className="editor-input" />}
+              placeholder={<Placeholder />}
+            />
+            <HistoryPlugin />
+            {/* <TreeViewPlugin /> */}
+            <AutoFocusPlugin />
+            <CodeHighlightPlugin />
+            <ListPlugin />
+            <LinkPlugin />
+            <AutoLinkPlugin />
+            <ListMaxIndentLevelPlugin maxDepth={7} />
+            <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+            <OnChangePlugin onChange={onChange} />
+          </div>
         </div>
-      </div>
-    </LexicalComposer>
-    // </Box>
+      </LexicalComposer>
+
+      <Button onClick={toggleSave}>{editable ? 'save' : 'edit'}</Button>
+    </Stack>
   )
 }
